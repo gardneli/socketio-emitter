33499ab2127e435774c6232d6cec6000a418af58
Leland G <lelandgardner@gmail.com>
2018-03-24 19:43:15 -0700

    Added html

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res) {
-  res.send('<h1>Hello Leland!</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3001, function() {
