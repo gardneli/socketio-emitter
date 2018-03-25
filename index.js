3a53e1ba12e4ef3cca09d4ccf38def67126ecd08
Leland G <lelandgardner@gmail.com>
2018-03-24 21:18:26 -0700

    Updating JSON

--- index.js
+++ index.js
@@ -2,7 +2,44 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const testJson = {'message': 'hello'};
+const testJson = {
+  "symbol": "AAPL",
+  "companyName": "Apple Inc.",
+  "primaryExchange": "Nasdaq Global Select",
+  "sector": "Technology",
+  "calculationPrice": "tops",
+  "open": 154,
+  "openTime": 1506605400394,
+  "close": 153.28,
+  "closeTime": 1506605400394,
+  "high": 154.80,
+  "low": 153.25,
+  "latestPrice": 158.73,
+  "latestSource": "Previous close",
+  "latestTime": "September 19, 2017",
+  "latestUpdate": 1505779200000,
+  "latestVolume": 20567140,
+  "iexRealtimePrice": 158.71,
+  "iexRealtimeSize": 100,
+  "iexLastUpdated": 1505851198059,
+  "delayedPrice": 158.71,
+  "delayedPriceTime": 1505854782437,
+  "previousClose": 158.73,
+  "change": -1.67,
+  "changePercent": -0.01158,
+  "iexMarketPercent": 0.00948,
+  "iexVolume": 82451,
+  "avgTotalVolume": 29623234,
+  "iexBidPrice": 153.01,
+  "iexBidSize": 100,
+  "iexAskPrice": 158.66,
+  "iexAskSize": 100,
+  "marketCap": 751627174400,
+  "peRatio": 16.86,
+  "week52High": 159.65,
+  "week52Low": 93.63,
+  "ytdChange": 0.3665,
+};
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
