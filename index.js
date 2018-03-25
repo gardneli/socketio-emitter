86ff2fd6677ac3433308ca7da7deac24dde4b816
Leland G <lelandgardner@gmail.com>
2018-03-24 20:39:23 -0700

    Added socket.io

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 /* Socket.io emitter */
-var app = require('express')();
-var http = require('http').Server(app);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
 http.listen(3001, function() {
